cd26b0b89b827f22795b556830a7b1de5262c404
root <ssongmantop@gmail.com>
2024-02-25 19:48:56 +0900

    Update 25.producer-spt-ack.py

--- kafka/PythonSample/25.producer-spt-ack.py
+++ kafka/PythonSample/25.producer-spt-ack.py
@@ -43,6 +43,6 @@ def producer(topic_name='edu-topic01-b', range_cnt=10000, ackValue='0'):
         
 if __name__ == '__main__':
     # 타픽명을 아규먼트 로 입력 받는다.
-    # producer(sys.argv[1], int(sys.argv[2]), sys.argv[3])
-    producer()
+    producer(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    # producer()
 
