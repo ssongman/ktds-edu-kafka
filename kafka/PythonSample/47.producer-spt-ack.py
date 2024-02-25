b662aaf81af451a64af0227110148002ab3a2593
root <ssongmantop@gmail.com>
2024-02-25 17:11:04 +0900

    Update 47.producer-spt-ack.py

--- kafka/PythonSample/47.producer-spt-ack.py
+++ kafka/PythonSample/47.producer-spt-ack.py
@@ -11,65 +11,43 @@ import time
 config  = configparser.ConfigParser()  ## 클래스 객체 생성
 config.read('./kafka/PythonSample/config.ini', encoding='utf-8')
 
-def producer(range_cnt=10000, ackValue='0'):
-    # bootstrap_servers=config["KAFKAINFO"]["bootstrap_servers"]
-    # sasl_plain_username=config["KAFKAINFO"]["sasl_plain_username"]
-    # sasl_plain_password=config["KAFKAINFO"]["sasl_plain_password"]
-    # topic_name=config["KAFKAINFO"]["topic_name_b"]
+def producer(range_cnt=10000):
+    bootstrap_servers=config["KAFKAINFO"]["bootstrap_servers"]
+    sasl_plain_username=config["KAFKAINFO"]["sasl_plain_username"]
+    sasl_plain_password=config["KAFKAINFO"]["sasl_plain_password"]
+    topic_name=config["KAFKAINFO"]["topic_name_b"]
     
-    # print(f"KafkaProducer...")
-    # producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
-    #                         security_protocol="SASL_PLAINTEXT",
-    #                         sasl_mechanism='SCRAM-SHA-512',
-    #                         ssl_check_hostname=True,
-    #                         sasl_plain_username=sasl_plain_username,
-    #                         sasl_plain_password=sasl_plain_password,
-    #                         acks=ackValue)
-
-
-    
-    bootstrap_servers='my-cluster.kafka.43.203.62.69.nip.io:32100'
-    sasl_plain_username='edu-user'
-    sasl_plain_password='oXTjENLJMvdKV6CbQmU2NX0e87Rezxhc'
-    topic_name='edu-topic01-b'
-    """
-    ex) topic_name : edu-topic01
-    """    
-
     print(f"KafkaProducer...")
     producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
                             security_protocol="SASL_PLAINTEXT",
                             sasl_mechanism='SCRAM-SHA-512',
                             ssl_check_hostname=True,
                             sasl_plain_username=sasl_plain_username,
-                            sasl_plain_password=sasl_plain_password,
-                            acks='0')
-    
+                            sasl_plain_password=sasl_plain_password)
 
 
-
-    # 10000건을 1초에 한번씩 발송
-    print(f"topicName[{topic_name}] range_cnt[{range_cnt}] ack[{ackValue}] Producing...")
-    for i in range(range_cnt):
-        print(i)
-        sleep(1)
-        producer.send(topic_name, b'{"eventName":"a","num":%d,"title":"a", "writeId":"", "writeName": "", "writeDate":"" }' % i)
-
-
-
-    # # 30000건 테스트
-    # print(f"topicName[{topic_name}] range_cnt[{range_cnt}] ack[{ackValue}] Producing...")
-    # start_time = time.time() # 시작시간
+    # # 10000건을 1초에 한번씩 발송
+    # print(f"topicName[{topic_name}] range_cnt[{range_cnt}] Producing...")
     # for i in range(range_cnt):
     #     print(i)
+    #     sleep(1)
     #     producer.send(topic_name, b'{"eventName":"a","num":%d,"title":"a", "writeId":"", "writeName": "", "writeDate":"" }' % i)
 
-    # end_time = time.time() # 종료시간
-    # print("duration time :", end_time - start_time)  # 현재시각 - 시작시간 = 실행 시간
+
+
+    # 30000건 테스트
+    print(f"topicName[{topic_name}] range_cnt[{range_cnt}] Producing...")
+    start_time = time.time() # 시작시간
+    for i in range(range_cnt):
+        print(i)
+        producer.send(topic_name, b'{"eventName":"a","num":%d,"title":"a", "writeId":"", "writeName": "", "writeDate":"" }' % i)
+
+    end_time = time.time() # 종료시간
+    print("duration time :", end_time - start_time)  # 현재시각 - 시작시간 = 실행 시간
 
         
 if __name__ == '__main__':
     # 타픽명을 아규먼트 로 입력 받는다.
-    producer(int(sys.argv[1]), sys.argv[2])
+    producer(int(sys.argv[1]))
     # producer()
 
